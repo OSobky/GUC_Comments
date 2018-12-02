9cd7f51b964a879401ffffcd447eaabf2ee3c827
OSobky <Omar_sobky@hotmail.com>
2018-12-02 19:18:37 +0200

    took the data coming from the FB api and inserted it in the DB

--- main.go
+++ main.go
@@ -6,43 +6,52 @@ import (
 
 	"log"
 
-	//"os"
-	//"reflect"
+	"os"
+	"reflect"
 
-	//	fb "github.com/huandu/facebook"
+	fb "github.com/huandu/facebook"
 	_ "github.com/lib/pq"
 )
 
-// var (
-// 	dbname     = os.Getenv("DATABASE_NAME")
-// 	dbpassword = os.Getenv("DATABASE_PASSWORD")
-// 	dbuser     = os.Getenv("DATABASE_USER")
-// 	dbhost     = os.Getenv("DATABASE_HOST")
-//	ac = os.Getenv("ACCESS_TOKEN")
-//)
-
-const (
-	dbname     = "GUC_Comments"
-	dbpassword = "secret"
-	dbuser     = "root"
-	dbhost     = "db"
+var (
+	dbname     = os.Getenv("DATABASE_NAME")
+	dbpassword = os.Getenv("DATABASE_PASSWORD")
+	dbuser     = os.Getenv("DATABASE_USER")
+	dbhost     = os.Getenv("DATABASE_HOST")
+	ac         = os.Getenv("ACCESS_TOKEN")
 )
 
+// const (
+// 	dbname     = "GUC_Comments"
+// 	dbpassword = "secret"
+// 	dbuser     = "root"
+// 	dbhost     = "db"
+// )
+
 func main() {
-	q := ` SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE'`
+	q := ` SELECT table_name 
+			FROM information_schema.tables
+			 WHERE table_schema='public' AND table_type='BASE TABLE'`
+	q1 := `SELECT CourseId,CourseName
+			FROM Courses `
 	qcreate := `CREATE TABLE Courses
 				(
 					CourseId SERIAL PRIMARY KEY,
-					CourseName VARCHAR(30)
+					CourseName TEXT
 
 				);
 
 				CREATE TABLE Comments
 				(
 					CourseId INTEGER REFERENCES Courses(CourseId),
-					Comment VARCHAR(50)
+					Comment TEXT
 
 				);`
+	qinsertCourses := `INSERT INTO Courses(CourseId ,CourseName ) VALUES
+						(DEFAULT,'CSEN 702 Microprocessors  '),
+						(DEFAULT,'CSEN 703 Analysis and Design of Algorithms '),
+						(DEFAULT,'CSEN 704 Advanced computer lab ');`
+
 	dbInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
 		dbhost, dbuser, dbpassword, dbname)
 
@@ -59,12 +68,13 @@ func main() {
 
 	log.Print("connected")
 
-	Result, err := db.Exec(qcreate)
-	if (err) != nil {
+	db.Exec(qcreate)
+
+	Result, err := db.Exec(qinsertCourses)
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Print(Result)
+	fmt.Println(Result)
 
 	rows, err := db.Query(q)
 	if (err) != nil {
@@ -79,53 +89,93 @@ func main() {
 		}
 		log.Printf("Table name is %s", tableName)
 	}
+	rows2, err := db.Query(q1)
+
+	for rows2.Next() {
+		var (
+			courseName string
+			courseid   int
+		)
+		if err := rows2.Scan(&courseid, &courseName); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("course name is %s and id is %d", courseName, courseid)
+	}
 	log.Printf("hey again")
 
-	// res, err := fb.Get("582313518881669_582751342171220/comments", fb.Params{
-	// 	"fields":       "message",
-	// 	"access_token": ac,
-	// })
+	res, err := fb.Get("582313518881669_582751342171220/comments", fb.Params{
+		"fields":       "message",
+		"access_token": ac,
+	})
 
-	// if (err) != nil {
-	// 	log.Fatal(err)
+	if (err) != nil {
+		log.Fatal(err)
 
-	// }
-	// s, _ := res["data"].([]interface{})
+	}
 
-	// for i := 0; i < reflect.ValueOf(s).Len(); i++ {
+	s, _ := res["data"].([]interface{})
 
-	// 	msg := s[i].(map[string]interface{})
+	for i := 0; i < reflect.ValueOf(s).Len(); i++ {
 
-	// 	fmt.Println(msg["message"])
-	// }
+		msg := s[i].(map[string]interface{})
 
-	// fmt.Println("")
+		qinsertcomment := fmt.Sprintf(`INSERT INTO Comments(CourseId, Comment) VALUES (1 ,'%s')`,
+			msg["message"])
 
-	// res2, _ := fb.Get("582313518881669_582750698837951/comments", fb.Params{
-	// 	"fields":       "message",
-	// 	"access_token": ac})
+		Result, err := db.Exec(qinsertcomment)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(Result)
+		fmt.Println(qinsertcomment)
+	}
 
-	// r, _ := res2["data"].([]interface{})
+	fmt.Println("")
 
-	// for i := 0; i < reflect.ValueOf(r).Len(); i++ {
+	res2, _ := fb.Get("582313518881669_582750698837951/comments", fb.Params{
+		"fields":       "message",
+		"access_token": ac})
 
-	// 	msg := r[i].(map[string]interface{})
+	r, _ := res2["data"].([]interface{})
 
-	// 	fmt.Println(msg["message"])
-	// }
+	for i := 0; i < reflect.ValueOf(r).Len(); i++ {
 
-	// res3, _ := fb.Get("582313518881669_582751015504586/comments", fb.Params{
-	// 	"fields":       "message",
-	// 	"access_token": ac})
+		msg := r[i].(map[string]interface{})
 
-	// v, _ := res3["data"].([]interface{})
+		qinsertcomment := fmt.Sprintf(`INSERT INTO Comments(CourseId, Comment) VALUES (2 ,'%s')`,
+			msg["message"])
 
-	// for i := 0; i < reflect.ValueOf(v).Len(); i++ {
+		Result, err := db.Exec(qinsertcomment)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(Result)
+		fmt.Println(qinsertcomment)
+	}
 
-	// 	msg := v[i].(map[string]interface{})
+	fmt.Println("")
 
-	// 	fmt.Println(msg["message"])
-	// }
+	res3, _ := fb.Get("582313518881669_582751015504586/comments", fb.Params{
+		"fields":       "message",
+		"access_token": ac})
+
+	v, _ := res3["data"].([]interface{})
+
+	for i := 0; i < reflect.ValueOf(v).Len(); i++ {
+
+		msg := v[i].(map[string]interface{})
+
+		qinsertcomment := fmt.Sprintf(`INSERT INTO Comments(CourseId, Comment) VALUES (3 ,'%s')`,
+			msg["message"])
+
+		Result, err := db.Exec(qinsertcomment)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(Result)
+		fmt.Println(qinsertcomment)
+	}
+	fmt.Println("3ash ya sobkyy")
 	/*	http.HandleFunc("/", defaultHandler) // default directory
 		http.HandleFunc("/MICRO", microHandler)
 		http.HandleFunc("/ANALYSIS", analysisHandler)
