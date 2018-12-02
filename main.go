25e2bcb12d04e7b23acf1baa48ca2a9cddaa89a9
OSobky <Omar_sobky@hotmail.com>
2018-12-02 04:38:57 +0200

    i added the for loop to print all the comments in each post and modified some varibles

--- main.go
+++ main.go
@@ -2,61 +2,53 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 
 	fb "github.com/huandu/facebook"
 )
 
+const ac = "EAAa6VkKVZBVUBAKZAZBpHTSn1nVG6ZBRLOyVWrAFPmBe7AJWr6RAH64fq7kaMHifjjTx8bCoIelxC3860gLRO7AozL1vZB3vPlZAyNzxmCS7Ln4ehhKgbOBm8LOyRx7ZB1UOlIHpddQZCZAir6BwC1JR1DbiUg5JNrH6HmdQYQbv5wTFGroNt9mSlW1SRzXCz1bEZD"
+
 func main() {
 	res, _ := fb.Get("582313518881669_582751342171220/comments", fb.Params{
 		"fields":       "message",
-		"access_token": "EAAFDrTDhvyMBAE7A6bIPt92NdOC0RVqDaHAGVbiWJeLNDATBZACvjaKZCOaNn9W2ZBAtNYQOPgouDVgGG1hcmcfESgts8KBSCNz7ZABzzqShi4omU2wyED1kBqieIClFTSMM88yHwaHidME04ajLpZCG311tSEjrOZBsaK0HLJPiGt8V6bEFtJb9ZAl2G37z54ZD",
+		"access_token": ac,
 	})
-	//var message string
-	x, mOk := res["data"].([]map[string]interface{})
-	s, sOk := res["data"].([]interface{})
 
-	if mOk {
-		y := x[0]
-		fmt.Println(y["message"])
-	} else {
-		if sOk {
-			msg := s[0].(map[string]interface{})
-			fmt.Println(msg["message"])
-		}
+	s, _ := res["data"].([]interface{})
+
+	for i := 0; i < reflect.ValueOf(s).Len(); i++ {
+
+		msg := s[i].(map[string]interface{})
+
+		fmt.Println(msg["message"])
 	}
 
-	fmt.Println()
+	fmt.Println("")
+
 	res2, _ := fb.Get("582313518881669_582750698837951/comments", fb.Params{
 		"fields":       "message",
-		"access_token": "EAAFDrTDhvyMBAE7A6bIPt92NdOC0RVqDaHAGVbiWJeLNDATBZACvjaKZCOaNn9W2ZBAtNYQOPgouDVgGG1hcmcfESgts8KBSCNz7ZABzzqShi4omU2wyED1kBqieIClFTSMM88yHwaHidME04ajLpZCG311tSEjrOZBsaK0HLJPiGt8V6bEFtJb9ZAl2G37z54ZD",
-	})
-	e, mOk := res2["data"].([]map[string]interface{})
-	r, sOk := res2["data"].([]interface{})
+		"access_token": ac})
 
-	if mOk {
-		y := e[0]
-		fmt.Println(y["message"])
-	} else {
-		if sOk {
-			msg := r[0].(map[string]interface{})
-			fmt.Println(msg["message"])
-		}
+	r, _ := res2["data"].([]interface{})
+
+	for i := 0; i < reflect.ValueOf(r).Len(); i++ {
+
+		msg := r[i].(map[string]interface{})
+
+		fmt.Println(msg["message"])
 	}
 
 	res3, _ := fb.Get("582313518881669_582751015504586/comments", fb.Params{
 		"fields":       "message",
-		"access_token": "EAAFDrTDhvyMBAE7A6bIPt92NdOC0RVqDaHAGVbiWJeLNDATBZACvjaKZCOaNn9W2ZBAtNYQOPgouDVgGG1hcmcfESgts8KBSCNz7ZABzzqShi4omU2wyED1kBqieIClFTSMM88yHwaHidME04ajLpZCG311tSEjrOZBsaK0HLJPiGt8V6bEFtJb9ZAl2G37z54ZD",
-	})
-	c, mOk := res3["data"].([]map[string]interface{})
-	v, sOk := res3["data"].([]interface{})
+		"access_token": ac})
 
-	if mOk {
-		y := c[0]
-		fmt.Println(y["message"])
-	} else {
-		if sOk {
-			msg := v[0].(map[string]interface{})
-			fmt.Println(msg["message"])
-		}
+	v, _ := res3["data"].([]interface{})
+
+	for i := 0; i < reflect.ValueOf(v).Len(); i++ {
+
+		msg := v[i].(map[string]interface{})
+
+		fmt.Println(msg["message"])
 	}
 }
